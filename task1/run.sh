8df0c320e2fe6391d1466b93251b67679c488b4a
daniil <suchkovdaniel@yandex.ru>
2022-03-06 20:32:25 +0300

    ready

--- task1/run.sh
+++ task1/run.sh
@@ -3,16 +3,16 @@
 for i in 1 2 3 4
 do
 case "$1" in
-"--input_folder") INPUT_FOLDER="$2";;
-"--extension") EXTENSION="$2";;
-"--backup_folder") BACKUP_FOLDER="$2";;
-"--backup_archive_name") BACKUP_ARCHIVE_NAME="$2";;
+	"--input_folder") INPUT_FOLDER="$2";;
+	"--extension") EXTENSION="$2";;
+	"--backup_folder") BACKUP_FOLDER="$2";;
+	"--backup_archive_name") BACKUP_ARCHIVE_NAME="$2";;
 esac
 shift
 shift
 done
-mkdir $backup_folder
-find $input_folder -name "*.$extension" \( -exec cp —parents '{}' "$backup_folder" \; \)
-tar -czf $backup_folder/../$backup_archive_name $backup_folder 2> "/dev/null"
+mkdir $BACKUP_FOLDER
+find $INPUT_FOLDER -name "*.$EXTENSION" \( -exec cp —parents '{}' "$BACKUP_FOLDER" \; \)
+tar -czf $BACKUP_FOLDER/../$BACKUP_ARCHIVE_NAME $BACKUP_FOLDER 2> "/dev/null"
 echo "done"
 
